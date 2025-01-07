1ce267f557a4084f3676ef3752e14b9dd358026b
Aref TB <92437542+ArefMYTB@users.noreply.github.com>
2025-01-07 19:25:12 +0330

    Add files via upload

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('api/dashboard/', views.get_dashboard_data, name='dashboard_data'),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -2,7 +2,30 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .models import TruckLog
 from .serializers import TruckLogSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 class TruckLogViewSet(ModelViewSet):
     queryset = TruckLog.objects.all()
     serializer_class = TruckLogSerializer
+
+
+@api_view(['GET'])
+def get_dashboard_data(request):
+    data = {
+        "total_20foot_entries": 800,
+        "total_fele_entries": 350,
+        "total_40foot_entries": 250,
+    }
+
+    total_entries = sum(data.values())
+
+    data_percentage = {key: (value / total_entries) * 100 for key, value in data.items()}
+
+    response_data = {
+        "getTotal": total_entries,
+        "entries": data,
+        "percentages": data_percentage
+    }
+
+    return Response(response_data)
