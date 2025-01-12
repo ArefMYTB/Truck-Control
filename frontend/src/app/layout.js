b00b969ea611e8859bc30d132290962d7149ff83
Aref TB <92437542+ArefMYTB@users.noreply.github.com>
2025-01-12 17:39:41 +0330

    Add files via upload

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -19,6 +19,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          href="https://cdn.jsdelivr.net/gh/rastikerdar/vazir-font/dist/font-face.css"
+          rel="stylesheet"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
