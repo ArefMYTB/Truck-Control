4c1db551b375a10795b580262979cd12021a807a
Aref TB <92437542+ArefMYTB@users.noreply.github.com>
2025-01-13 16:31:01 +0330

    Add files via upload

--- frontend/src/app/Constants/images.js
+++ frontend/src/app/Constants/images.js
@@ -1,7 +1,17 @@
 import user from '../assets/images/luffy.jpg'
 import logo from '../assets/images/logo.jpg'
+import lp from '../assets/images/lp.png'
+import cp from '../assets/images/cp.png'
+import truck from '../assets/images/truck.jpg'
+import backtruck from '../assets/images/backtruck.jpg'
+import driver from '../assets/images/driver.jpg'
 
 export default {
     user,
     logo,
+    lp,
+    cp,
+    truck,
+    backtruck,
+    driver
 }
