8965b7a6e18a670e4bf7c69ed3f9b02bfed61d8c
Aref TB <92437542+ArefMYTB@users.noreply.github.com>
2025-03-17 15:13:32 +0330

    Get Camera Videos

---
+++ frontend/public/container_camera_stream.sh
@@ -0,0 +1,8 @@
+while true; do
+    ffmpeg -rtsp_transport tcp -i "rtsp://admin:Admin@123@192.168.10.11:554" \
+        -vf scale=640:480 \
+        -c:v libx264 -preset ultrafast -crf 30 -f hls \
+        -hls_time 2 -hls_list_size 5 -hls_flags delete_segments+append_list stream_container.m3u8
+
+    echo "FFmpeg crashed. Restarting..."
+done
